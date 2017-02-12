106f48e7ed5f7f1b953599ecd89d0e6cb2a6b7b1
John Zeiders <zeiders@usc.edu>
2017-02-11 16:20:41 -0800

    Merged Caleb's Junk

--- website/src/app.jsx
+++ website/src/app.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PlayerContainer from "./player/playerContainer.jsx";
-import { Provider } from 'react-redux';
-import reducer from './reducers'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux';
+import reducers from './reducers'
 
-
+const store = createStore(reducers,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default class App extends React.Component {
     constructor(props) {
@@ -11,11 +12,13 @@ export default class App extends React.Component {
     }
     render() {
         return (
-            <Provider>
-                <h1>
-                    Spotify App
-                </h1>
-                <PlayerContainer/>
+            <Provider store={store}>
+                <div>
+                    <h1>
+                        Spotify App
+                    </h1>
+                    <PlayerContainer/>
+                </div>
             </Provider>
         )
     }
