20d152e15e936860efc9f19d5596206304d48296
linux-jedi <calebtho@usc.edu>
2017-02-11 17:52:35 -0800

    SQL migrations, preparing for heroku

--- spotify-integration/flask-server/app/__init__.py
+++ spotify-integration/flask-server/app/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask_cors import CORS, cross_origin
 from app.database import db_session
-from app import views, models
+
 
 app = Flask(__name__)
 CORS(app)
@@ -11,3 +11,5 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
+
+from app import views, models
--- spotify-integration/flask-server/app/models.py
+++ spotify-integration/flask-server/app/models.py
@@ -1,15 +1,22 @@
 from sqlalchemy import Column, Integer, String, JSON
-from yourapplication.database import Base
+from app.database import Base
 
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True)
-    email = Column(String(120), unique=True)
+    playlists = Column(JSON)
 
-    def __init__(self, name=None, email=None):
+    def __init__(self, name=None, playlists=None):
         self.name = name
-        self.email = email
+        playlists["categories"] = []
+
 
     def __repr__(self):
         return '<User %r>' % (self.name)
+
+class TrainingData(Base):
+    __tablename__ = "training_data"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True)
+    data = Column(JSON)
